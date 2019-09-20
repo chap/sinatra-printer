5f223fb17586450f9e201ea12e0ad0678cfd991b
Chap Ambrose <chapambrose@gmail.com>
2019-09-20 16:10:07 +0000

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require "sinatra"
 
 get '/*' do
-  sleep(40)
-  puts params.to_s
-  puts headers.to_s
-  return params.to_s
+#   sleep(40)
+#   puts params.to_s
+#   puts headers.to_s
+#   return params.to_s
+  return 'success!'
 end
 
 
