9f0b6cfeff533756e6a8805cd8b4d47bfe6d8fb1
Chap Ambrose <cambrose@salesforce.com>
2020-12-18 10:53:40 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,8 +4,15 @@ get '/*' do
   #sleep(40)
   puts params.to_s
   puts headers.to_s
-  puts request.inspect
-#   return params.to_s
+  puts request.body
+  return 'success!'
+end
+
+post '/*' do
+  #sleep(40)
+  puts params.to_s
+  puts headers.to_s
+  puts request.body
   return 'success!'
 end
 
