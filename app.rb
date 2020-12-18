acb731012eb9e935e4e7460b137f8c7abb613490
Chap Ambrose <cambrose@salesforce.com>
2020-12-18 10:12:51 -0600

    print request

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require "sinatra"
 
 get '/*' do
-#   sleep(40)
-#   puts params.to_s
-#   puts headers.to_s
+  sleep(40)
+  puts params.to_s
+  puts headers.to_s
+  puts request.inspect
 #   return params.to_s
   return 'success!'
 end
