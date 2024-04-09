ec1d6105f63c26d2972ce43712f2b69de1a2f4ac
Chap Ambrose <cambrose@salesforce.com>
2024-04-09 16:10:24 -0500

    try sqs queue

--- .heroku/workflow-runner/main.tf
+++ .heroku/workflow-runner/main.tf
@@ -1,9 +1,20 @@
 locals {
-    foo = "bar"
+  cluster_name = "shared-services-18498"
+  repo_name = "chap/sinatra-printer"
+}
+
+terraform {
+    backend "s3"{
+        bucket = "tofu-states-${local.cluster_name}"
+        key    = "${local.repo_name}/.heroku/workflow-runner/tofu_state.json"
+        # region = "{{.BackendS3Region}}"
+        # dynamodb_table = "{{.BackendDynamoDbTable}}"
+        # profile = "backend"
+    }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_sqs_queue" "terraform_queue" {
