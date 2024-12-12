c7fbf193dfa335debe53f48f7a08dbecf8f357f6
Chap Ambrose <cambrose@salesforce.com>
2024-12-12 19:48:46 +0000

    pretty print

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
+ENV['COLOR'] ||= 'white'
 require "sinatra"
+require "pp"
 
 get '/*' do
   #sleep(40)
   puts params.to_s
   puts headers.to_s
   puts request.body.read.to_s
-  return 'success!'
+  return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{ JSON.pretty_generate({params:, headers:headers.to_h, requestBody:request.body})}</pre></html>"
 end
 
 post '/*' do
