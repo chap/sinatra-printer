f4f0514b2fe83949392ccd6d2080766943571b1b
Chap Ambrose <cambrose@salesforce.com>
2024-12-13 03:37:44 +0000

    ipv6

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: bundle exec ruby app.rb -p $PORT
-worker: bundle exec ruby worker.rb
+# web: bundle exec ruby app.rb -p $PORT -b [::] 
+web: bundle exec puma config.ru -C puma.rb
+# worker: bundle exec ruby worker.rb
--- app.rb
+++ app.rb
@@ -2,22 +2,22 @@ ENV['COLOR'] ||= 'white'
 require "sinatra"
 require "pp"
 
-get '/*' do
-  #sleep(40)
-  puts params.to_s
-  puts headers.to_s
-  puts request.body.read.to_s
-  return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{ JSON.pretty_generate({params:, headers:headers.to_h, requestBody:request.body})}</pre></html>"
-end
+class Application < Sinatra::Base
 
-post '/*' do
-  #sleep(40)
-  puts params.to_s
-  puts headers.to_s
-  puts request.inspect
-  puts 'body!'
-  puts request.body.read
-  return 'success!'
-end
+  get '/*' do
+    #sleep(40)
+    puts params.to_s
+    puts headers.to_s
+    puts request.body.read.to_s
+    return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{ JSON.pretty_generate({params:, headers:headers.to_h, requestBody:request.body})}</pre></html>"
+  end
 
-# bump
+  post '/*' do
+    #sleep(40)
+    puts params.to_s
+    puts headers.to_s
+    puts request.body.read.to_s
+    return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{ JSON.pretty_generate({params:, headers:headers.to_h, requestBody:request.body})}</pre></html>"
+  end
+
+end
