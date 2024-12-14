d2eb2715b9639339cfb283f3f9df93081a193265
Chap Ambrose <cambrose@salesforce.com>
2024-12-14 22:55:58 +0000

    dont print env

--- app.rb
+++ app.rb
@@ -5,13 +5,13 @@ require "pp"
 class Application < Sinatra::Base
 
   get '/*' do
-    payload = JSON.pretty_generate({params:, headers:request.env.to_h, requestBody:request.body.read.to_s})
+    payload = JSON.pretty_generate({params:, requestBody:request.body.read.to_s})
     puts payload
     return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{payload}</pre></html>"
   end
 
   post '/*' do
-    payload = JSON.pretty_generate({params:, headers:request.env.to_h, requestBody:request.body.read.to_s})
+    payload = JSON.pretty_generate({params:, requestBody:request.body.read.to_s})
     puts payload
     return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{payload}</pre></html>"
   end
