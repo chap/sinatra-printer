52fee7e0a8e0f0b20d289a46d26250937d94c5cc
Chap Ambrose <cambrose@salesforce.com>
2024-12-14 13:46:34 +0000

    print payload to logs

--- app.rb
+++ app.rb
@@ -5,11 +5,15 @@ require "pp"
 class Application < Sinatra::Base
 
   get '/*' do
-    return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{ JSON.pretty_generate({params:, headers:request.env.to_h, requestBody:request.body.read.to_s})}</pre></html>"
+    payload = JSON.pretty_generate({params:, headers:request.env.to_h, requestBody:request.body.read.to_s})
+    puts payload
+    return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{payload}</pre></html>"
   end
 
   post '/*' do
-    return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{ JSON.pretty_generate({params:, headers:request.env.to_h, requestBody:request.body.read.to_s})}</pre></html>"
+    payload = JSON.pretty_generate({params:, headers:request.env.to_h, requestBody:request.body.read.to_s})
+    puts payload
+    return "<html style='font:monospace;background:#{ENV['COLOR']}'><pre>#{payload}</pre></html>"
   end
 
 end
